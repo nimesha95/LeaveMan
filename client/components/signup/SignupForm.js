f075ea940ae0a5fe8b83b28908800f3d518bed60
nimesha95 <nimesha95@live.com>
2018-05-25 21:39:15 +0530

    added serverside validation

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,51 +1,82 @@
 import React from "react";
 import { hot } from "react-hot-loader";
-import {PropTypes} from 'prop-types';
+import { PropTypes } from "prop-types";
+import classnames from 'classnames';
 
 class SignupForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            passwordConfirmation: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: {},
+      isLoading: false
+    };
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    onChange(e){
-        this.setState({[e.target.name] : e.target.value});
-    }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    onSubmit(e){
-        e.preventDefault();
-        this.props.userSignupRequest(this.state);
-    }
+  onSubmit(e) {
+    this.setState({isLoading:true , errors: {} });
+    e.preventDefault();
+    this.props
+      .userSignupRequest(this.state)
+      .then(
+        () => {},
+        ({ response }) => this.setState({ errors: response.data , isLoading:false})
+      );
+  }
 
   render() {
+    const { errors } = this.state;
     return (
       <form onSubmit={this.onSubmit}>
         <h1>Join US </h1>
 
-        <div className="form-group">
+        <div className={classnames("form-group",{'has-error': errors.username})}>
           <label className="control-label">Username</label>
-          <input type="text" value={this.state.username} onChange={this.onChange} name="username" className="form-control" />
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.onChange}
+            name="username"
+            className="form-control"
+          />
+          {errors.passwordConfirmation && <span className="help-block">{errors.username}</span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group",{'has-error': errors.password})}>
           <label className="control-label">Password</label>
-          <input type="password" value={this.state.password} onChange={this.onChange} name="password" className="form-control" />
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.onChange}
+            name="password"
+            className="form-control"
+          />
+          {errors.passwordConfirmation && <span className="help-block">{errors.password}</span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group",{'has-error': errors.passwordConfirmation})}>
           <label className="control-label">Retype Password</label>
-          <input type="password" value={this.state.passwordConfirmation} onChange={this.onChange} name="passwordConfirmation" className="form-control" />
+          <input
+            type="password"
+            value={this.state.passwordConfirmation}
+            onChange={this.onChange}
+            name="passwordConfirmation"
+            className="form-control"
+          />
+          {errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
         </div>
 
         <div className="form-group">
-          <button className="btn btn-primary btn-lg">Signup</button>
+          <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">Signup</button>
         </div>
       </form>
     );
@@ -53,7 +84,7 @@ class SignupForm extends React.Component {
 }
 
 SignupForm.propTypes = {
-    userSignupRequest: PropTypes.func.isRequired
-}
+  userSignupRequest: PropTypes.func.isRequired
+};
 
 export default hot(module)(SignupForm);
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from 'path';
+var bodyParser = require('body-parser');
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
+
+var users = require('./routes/users');
 
 let app = express();
 
@@ -17,8 +20,12 @@ app.use(webpackMiddleware(compiler,{
 }))
 app.use(webpackHotMiddleware(compiler))
 
+app.use(bodyParser.json());
+
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'./index.html'));
 });
+
+app.use('/api/users', users);
 
 app.listen(3000,() => console.log("Server is running at port 3000"))
